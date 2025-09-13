9b42c0ef856b0e916cc3eb989cd416066d9bfb97
Iury <iurysena74@gmail.com>
2025-09-13 09:19:49 -0300

    feat: add projects constant

---
+++ src/constants/projects.ts
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    title: "Projeto Final InfoJR 2025.1",
+    description:
+      "Aplicação web desenvolvida como projeto final de trainee da InfoJR, utilizando Next.js e integração com backend via APIs RESTful.",
+    image: "/assets/infojr.png",
+    technologies: ["Next.js", "TailwindCSS", "Prismic CMS"],
+    liveUrl: "",
+    githubUrl: "https://gitlab.com/projetofinalpluginfo2025.1/frontend/",
+  },
+  {
+    title: "Uolha Isso",
+    description:
+      "Plataforma para exibição de fotoreportagens, desenvolvida como projeto acadêmico no IFBA.",
+    image: "/assets/uolhaisso.png",
+    technologies: ["Next.js", "TailwindCSS", "Prisma", "UploadThing"],
+    liveUrl: "https://uolhaisso.vercel.app/",
+    githubUrl: "https://github.com/iuryyxd/uolhaisso",
+  },
+  {
+    title: "Finance AI",
+    description:
+      "Plataforma para organização e controle de finanças, desenvolvida durante a FullStackWeek.",
+    image: "/assets/finance-ai.png",
+    technologies: ["Next.js", "TypeScript", "TailwindCSS", "Clerk"],
+    liveUrl: "https://finance-ai-orcin.vercel.app/",
+    githubUrl: "https://github.com/iuryyxd/finance-ai",
+  },
+  {
+    title: "Note.me",
+    description:
+      "Aplicação web para criação e gerenciamento de notas com sistema de login usando Firebase.",
+    image: "/assets/noteme.png",
+    technologies: ["Next.js", "TailwindCSS", "TypeScript", "Firebase"],
+    liveUrl: "https://noteme.vercel.app/",
+    githubUrl: "https://github.com/iuryyxd/note.me",
+  },
+];
